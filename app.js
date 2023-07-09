1d9f97a09d38c8dda140bb523a245d5e071e12d5
parm-is-coding <pardhi1400@gmail.com>
2023-07-09 16:53:08 -0700

    add book object constructor

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+// Book Object constructor
+function Book(title,author,pages,read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read =read;
+    this.info = function() {
+        let output = this.title + " by " + this.author +
+        ", " + this.pages + ", " + this.read;
+        return output;
+    }
+}
